df7aadae53c477b3308d13b080b2ae2538cf42ea
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-04-05 12:26:07 -0700

    Add support for the ISC and MIT license

--- web.py
+++ web.py
@@ -1,15 +1,23 @@
 import urllib
 import os
-from flask import Flask, jsonify
+import github
+from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
 @app.route("/<user>/<repo>")
 def index_with_url(user, repo):
+    try:
+        license_type = github.get_license(user, repo)
+    except (KeyError, ValueError) as e:
+        resp = jsonify(message=unicode(e))
+        resp.status_code = 400
+        return resp
+ 
     return jsonify({
-        "user": user,
-        "repo": repo,
-        "license": "",
+        'user': user,
+        'repo': repo,
+        'license': license_type,
         })
 
 if __name__ == '__main__':
